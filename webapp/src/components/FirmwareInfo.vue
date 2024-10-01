d7d1d19dfb47a00b53b5e2f65a3540fef5d9548c
marco <marco.pilz@ifm.com>
2024-10-01 09:04:10 +0200

    feat: add duo fusion configurations
    chore: Update PR template link to point to the correct repository
    
    fix: yarn prettier

--- webapp/src/components/FirmwareInfo.vue
+++ webapp/src/components/FirmwareInfo.vue
@@ -18,12 +18,8 @@
                     <tr>
                         <th>{{ $t('firmwareinfo.FirmwareVersion') }}</th>
                         <td>
-                            <a
-                                :href="versionInfoUrl"
-                                target="_blank"
-                                v-tooltip
-                                :title="$t('firmwareinfo.FirmwareVersionHint')"
-                            >
+                            <a :href="versionInfoUrl" target="_blank" v-tooltip
+                                :title="$t('firmwareinfo.FirmwareVersionHint')">
                                 {{ systemStatus.git_hash }}
                             </a>
                         </td>
@@ -40,31 +36,18 @@
                         <th>{{ $t('firmwareinfo.FirmwareUpdate') }}</th>
                         <td>
                             <div class="form-check form-check-inline form-switch">
-                                <input
-                                    v-model="modelAllowVersionInfo"
-                                    class="form-check-input"
-                                    type="checkbox"
-                                    role="switch"
-                                    v-tooltip
-                                    :title="$t('firmwareinfo.FrmwareUpdateAllow')"
-                                />
+                                <input v-model="modelAllowVersionInfo" class="form-check-input" type="checkbox"
+                                    role="switch" v-tooltip :title="$t('firmwareinfo.FrmwareUpdateAllow')" />
                                 <label class="form-check-label">
-                                    <a
-                                        v-if="modelAllowVersionInfo && systemStatus.update_url !== undefined"
-                                        :href="systemStatus.update_url"
-                                        target="_blank"
-                                        v-tooltip
-                                        :title="$t('firmwareinfo.FirmwareUpdateHint')"
-                                    >
+                                    <a v-if="modelAllowVersionInfo && systemStatus.update_url !== undefined"
+                                        :href="systemStatus.update_url" target="_blank" v-tooltip
+                                        :title="$t('firmwareinfo.FirmwareUpdateHint')">
                                         <span class="badge" :class="systemStatus.update_status">
                                             {{ systemStatus.update_text }}
                                         </span>
                                     </a>
-                                    <span
-                                        v-else-if="modelAllowVersionInfo"
-                                        class="badge"
-                                        :class="systemStatus.update_status"
-                                    >
+                                    <span v-else-if="modelAllowVersionInfo" class="badge"
+                                        :class="systemStatus.update_status">
                                         {{ systemStatus.update_text }}
                                     </span>
                                 </label>
